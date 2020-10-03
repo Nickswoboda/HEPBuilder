0d2897ff9b8efb0b941e374dd397b876db77d859
Nick Swoboda <nickswoboda@gmail.com>
2020-10-03 09:54:30 -0700

    Improve look of tooltip

--- src/tooltip.cpp
+++ src/tooltip.cpp
@@ -5,11 +5,22 @@
 
 Tooltip::Tooltip(QWidget *parent) : QWidget(parent)
 {
+	setStyleSheet("background: aliceblue; border: 1px solid black");
+    setAttribute(Qt::WA_TransparentForMouseEvents);
+
     QVBoxLayout* layout = new QVBoxLayout(this);
+    layout->setAlignment(Qt::AlignTop);
+    layout->setMargin(0);
+    layout->setSpacing(0);
     setLayout(layout);
 
-    QLabel* name = new QLabel("name");
-    layout->addWidget(name);
+    name_label_ = new QLabel("Name");
+    name_label_->setWordWrap(true);
+    name_label_->setStyleSheet("background: #B0C4DE; font-weight: bold");
 
+    instruction_label_ = new QLabel("Instruction");
+    instruction_label_->setWordWrap(true);
 
+    layout->addWidget(name_label_);
+	layout->addWidget(instruction_label_);
 }
--- src/tooltip.h
+++ src/tooltip.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <QWidget>
+#include <QLabel>
 
 class Tooltip : public QWidget
 {
 public:
     explicit Tooltip(QWidget *parent = nullptr);
+
+	QLabel* name_label_;
+	QLabel* instruction_label_;
 
 };
 
