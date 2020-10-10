5783d48d4e4bc0542f08ff257d3dbeb1e9f9017b
Nick Swoboda <nickswoboda@gmail.com>
2020-10-09 18:33:56 -0700

    Add basic printing support

--- src/preview_window.cpp
+++ src/preview_window.cpp
@@ -1,6 +1,10 @@
 #include "preview_window.h"
 #include "exercise_card.h"
 
+#include <QtPrintSupport/QPrintPreviewDialog>
+#include <QPainter>
+#include <iostream>
+
 PreviewWindow::PreviewWindow(const std::vector<Exercise*>& exercises, QWidget* parent)
     :QDialog(parent)
 {
@@ -10,13 +14,35 @@ PreviewWindow::PreviewWindow(const std::vector<Exercise*>& exercises, QWidget* p
     v_box_ = new QVBoxLayout();
     widget->setLayout(v_box_);
     v_box_->setAlignment(Qt::AlignTop);
-
-
     ui_.preview_area->setWidget(widget);
     for (const auto& ex : exercises){
         ExerciseCard* card = new ExerciseCard(*ex, widget);
         widget->layout()->addWidget(card);
     }
+
+    connect(ui_.buttonBox, SIGNAL(accepted()), this, SLOT(OnPrintPressed()));
 }
 
+void PreviewWindow::OnPrintPressed()
+{
+    QPrintPreviewDialog* preview = new QPrintPreviewDialog(this);
+    connect(preview, SIGNAL(paintRequested(QPrinter*)), this, SLOT(DrawPrintPreview(QPrinter*)));
+    preview->exec();
+}
 
+void PreviewWindow::DrawPrintPreview(QPrinter* printer)
+{
+    QPainter painter(printer);
+    const int ex_per_page = 7;
+
+    for (int i = 0; i < v_box_->count(); ++i){
+        QWidget* card = v_box_->itemAt(i)->widget();
+        card->render(&painter);
+        painter.translate(0, 180);
+        if (i == ex_per_page){
+            printer->newPage();
+        //move painter back to top of page
+            painter.translate(0, -180 * 8);
+        }
+    }
+}
--- src/preview_window.h
+++ src/preview_window.h
@@ -4,6 +4,7 @@
 
 #include <QDialog>
 #include <QVBoxLayout>
+#include <QtPrintSupport/QPrinter>
 
 #include <vector>
 
@@ -11,9 +12,16 @@
 
 class PreviewWindow : public QDialog
 {
+    Q_OBJECT
 public:
     PreviewWindow(const std::vector<Exercise*>& exercises, QWidget* parent);
+    virtual ~PreviewWindow() {};
 
+public slots:
+    void OnPrintPressed();
+    void DrawPrintPreview(QPrinter* printer);
+
+private:
     Ui::PreviewWindow ui_;
     QVBoxLayout* v_box_;
 };
